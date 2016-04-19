ca23c0ff5e32bf6b82d1f194c8b8753e2f80f25d
Evan Mattson <me@aaemnnost.tv>
2016-04-18 22:54:41 -0400

    formatting

--- src/WordPressTestsInstaller.php
+++ src/WordPressTestsInstaller.php
@@ -10,102 +10,112 @@ class WordPressTestsInstaller extends LibraryInstaller
 
     const TYPE = 'wordpress-tests';
 
-    private static $installed = array();
+    private static $installed = [];
 
     /**
      * {@inheritDoc}
      */
     public function getInstallPath(PackageInterface $package)
     {
-		/**
-		 * Set install path from root
-		 */
-		 $path = $this->getRootInstallPath();
+        /**
+         * Set install path from root
+         */
+        $path = $this->getRootInstallPath();
 
-		/**
-		 * Set install path via package
-		 */
-		if (! $path && $package->getExtra()) {
+        /**
+         * Set install path via package
+         */
+        if ( ! $path && $package->getExtra()) {
             $path = $this->getInstallPathFromExtra($package->getExtra());
         }
 
-		$this->recordInstallPath($package, $path);
+        $this->recordInstallPath($package, $path);
 
         return $path;
     }
 
-	/**
-	 * [recordInstallPath description]
-	 * @param  PackageInterface $package [description]
-	 * @param  [type]           $path    [description]
-	 * @return [type]                    [description]
-	 */
-	protected function recordInstallPath(PackageInterface $package, $path)
-	{
-		$this->guardAgainstPathConflicts($package, $path);
+    /**
+     * [recordInstallPath description]
+     *
+     * @param  PackageInterface $package [description]
+     * @param  [type]           $path    [description]
+     *
+     * @return [type]                    [description]
+     */
+    protected function recordInstallPath(PackageInterface $package, $path)
+    {
+        $this->guardAgainstPathConflicts($package, $path);
 
-		self::$installed[ $path ] = $package;
-	}
+        self::$installed[ $path ] = $package;
+    }
 
-	/**
-	 * [guardAgainstPathConflicts description]
-	 * @param  PackageInterface $package [description]
-	 * @param  [type]           $path    [description]
-	 * @return [type]                    [description]
-	 */
-	protected function guardAgainstPathConflicts(PackageInterface $package, $path)
-	{
-		if ($this->isPackageConflictForPath($package, $path)) {
-			throw new \InvalidArgumentException('Two packages cannot share the same directory!');
-		}
-	}
+    /**
+     * [guardAgainstPathConflicts description]
+     *
+     * @param  PackageInterface $package [description]
+     * @param  [type]           $path    [description]
+     *
+     * @return [type]                    [description]
+     */
+    protected function guardAgainstPathConflicts(PackageInterface $package, $path)
+    {
+        if ($this->isPackageConflictForPath($package, $path)) {
+            throw new \InvalidArgumentException('Two packages cannot share the same directory!');
+        }
+    }
 
-	/**
-	 * [isPackageConflictForPath description]
-	 * @param  PackageInterface $package [description]
-	 * @param  [type]           $path    [description]
-	 * @return boolean                   [description]
-	 */
-	protected function isPackageConflictForPath(PackageInterface $package, $path)
-	{
-		return $this->hasPackageForPath($path)
-		      && ($package->getPrettyName() != self::$installed[ $path ]->getPrettyName());
-	}
+    /**
+     * [isPackageConflictForPath description]
+     *
+     * @param  PackageInterface $package [description]
+     * @param  [type]           $path    [description]
+     *
+     * @return boolean                   [description]
+     */
+    protected function isPackageConflictForPath(PackageInterface $package, $path)
+    {
+        return $this->hasPackageForPath($path)
+               && ($package->getPrettyName() != self::$installed[ $path ]->getPrettyName());
+    }
 
-	/**
-	 * [hasPackageForPath description]
-	 * @param  [type]  $path [description]
-	 * @return boolean       [description]
-	 */
-	protected function hasPackageForPath($path)
-	{
-		return ! empty(self::$installed[ $path ]);
-	}
+    /**
+     * [hasPackageForPath description]
+     *
+     * @param  [type]  $path [description]
+     *
+     * @return boolean       [description]
+     */
+    protected function hasPackageForPath($path)
+    {
+        return ! empty(self::$installed[ $path ]);
+    }
 
-	/**
-	 * [getRootInstallPath description]
-	 * @return [type] [description]
-	 */
-	protected function getRootInstallPath()
-	{
-		if (! $this->composer->getPackage()) {
-			return;
-		}
+    /**
+     * [getRootInstallPath description]
+     * @return [type] [description]
+     */
+    protected function getRootInstallPath()
+    {
+        if ( ! $this->composer->getPackage()) {
+            return;
+        }
 
-		return $this->getInstallPathFromExtra($this->composer->getPackage()->getExtra());
-	}
+        return $this->getInstallPathFromExtra($this->composer->getPackage()->getExtra());
+    }
 
-	/**
-	 * [getInstallPathFromExtra description]
-	 * @param  [type] $extra [description]
-	 * @return [type]        [description]
-	 */
-	protected function getInstallPathFromExtra($extra)
-	{
-		return ! empty($extra['wordpress-tests-dir'])
-			? $extra['wordpress-tests-dir']
-			: null;
-	}
+    /**
+     * [getInstallPathFromExtra description]
+     *
+     * @param  [type] $extra [description]
+     *
+     * @return [type]        [description]
+     */
+    protected function getInstallPathFromExtra($extra)
+    {
+        return ! empty($extra[ 'wordpress-tests-dir' ])
+            ? $extra[ 'wordpress-tests-dir' ]
+            : null;
+    }
 
     /**
      * {@inheritDoc}
